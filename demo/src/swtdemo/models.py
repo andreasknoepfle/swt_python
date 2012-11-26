03bd1b8376b69637e19f6b637cbb05a4775a0536
basa <basa@example.org>
2012-11-26 21:02:26 +0100

    updated demo

---
+++ demo/src/swtdemo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Concern(models.Model):
+   name = models.CharField(max_length=40)
+
+class Employee(models.Model):
+   name = models.CharField(max_length=60)
+   boss = models.BooleanField(default=False)
+   concern = models.ForeignKey(Concern)
+
